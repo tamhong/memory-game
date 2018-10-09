8dedbaa12a62ba4e64f56f6a4b410ff3b873c797
Tam Hong <tamhong.sd@gmail.com>
2018-10-09 12:28:28 -0700

    Updates

---
+++ precache-manifest.dd1d05e52303e350e82c86b91e102a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "eae7558ba577c4fe868f",
+    "url": "/memory-game/static/js/main.eae7558b.chunk.js"
+  },
+  {
+    "revision": "a7d24ebd41258dcfb10a",
+    "url": "/memory-game/static/js/1.a7d24ebd.chunk.js"
+  },
+  {
+    "revision": "eae7558ba577c4fe868f",
+    "url": "/memory-game/static/css/main.af85a2dd.chunk.css"
+  },
+  {
+    "revision": "a1ae4c70cdc7839b92e46668b943a880",
+    "url": "/memory-game/index.html"
+  }
+];
